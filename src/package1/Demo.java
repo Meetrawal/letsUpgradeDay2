e93daf309e23a4b6571a72d94edeb9d9b481e36e
Meetrawal <mnrawal55@gmail.com>
2020-10-14 17:31:50 +0530

    Add files via upload

---
+++ src/package1/Demo.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class Demo {
+	static int a=2;
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		e1.name="Meet Rawal";
+		e1.age=23;
+		e1.city="Kalyan";
+		e1.display();
+		
+		Employee e2 = new Employee();
+		e2.name="Hitesh Sakhare";
+		e2.age=22;
+		e2.city="Mumbai";
+		e2.display();
+		
+		System.out.println("==========================================");
+
+	}
+}
---
+++ src/package1/Employee.java
@@ -0,0 +1,15 @@
+package package1;
+public class Employee {
+	String name;
+	int age;
+	String city;
+	
+	void display() {
+		System.out.println("==========================================");
+		System.out.println("The Employee Name is "+name+".");
+		System.out.println("Age - "+age);
+		System.out.println("City - "+city);
+
+	}
+
+}
